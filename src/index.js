b2685f996e36f92f02538faa852fb6b6a586a083
SWDevStudio <62483809+SWDevStudio@users.noreply.github.com>
2020-04-20 15:50:26 +0300

    Готовая конфигурация для верстки, все scss файлы из service подключаются автоматически.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Подключение плагинов и библиотек
+import 'owl.carousel'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+
+
+
+
+function requireAll(requireContext) {
+    return requireContext.keys().map(requireContext);
+}
+
+// Подключать все компоненты
+requireAll(require.context('./components', true, /\.(scss)$/));
+requireAll(require.context('./components', true, /\.(jsx?)$/));
+requireAll(require.context('./service', true, /\.(scss)$/));
